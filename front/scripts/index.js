975e53c8b69dd8f31b3b3efd7dbcba31d628f678
pandreasantacruz <andreasantacruz123@gmail.com>
2024-12-18 10:20:56 -0600

    TareaDos

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,37 +1,35 @@
-const divTarjetas = document.getElementById('misPeliculasFavoritas')
+const divTarjetas = document.getElementById("misPeliculasFavoritas");
 
-const mapear = ((movie) => {
-    const div = document.createElement("div")
-    const titulo = document.createElement("strong")
-    const ano = document.createElement("span")
-    const direc = document.createElement("span")
-    const duracion = document.createElement("span")
-    const puntuacion = document.createElement("span")
-    const imagen = document.createElement("img")
+const mapear = (movie) => {
+  const div = document.createElement("div");
+  const titulo = document.createElement("strong");
+  const ano = document.createElement("span");
+  const direc = document.createElement("span");
+  const duracion = document.createElement("span");
+  const puntuacion = document.createElement("span");
+  const imagen = document.createElement("img");
 
-    imagen.src = movie.poster
-    imagen.alt = movie.title
-    puntuacion.innerText = movie.rate
-    duracion.innerText = movie.duration
-    direc.innerText = movie.director
-    ano.innerText = movie.year
-    titulo.innerText = movie.title  
+  imagen.src = movie.poster;
+  imagen.alt = movie.title;
+  puntuacion.innerText = movie.rate;
+  duracion.innerText = movie.duration;
+  direc.innerText = movie.director;
+  ano.innerText = movie.year;
+  titulo.innerText = movie.title;
 
-    div.appendChild(titulo)
-    div.appendChild(ano)
-    div.appendChild(direc)
-    div.appendChild(duracion)
-    div.appendChild(puntuacion)
-    div.appendChild(imagen)
-    
-    
-    return div
- 
-})
-const arrayHtmlElement = tempData.map(mapear);
+  div.appendChild(titulo);
+  div.appendChild(ano);
+  div.appendChild(direc);
+  div.appendChild(duracion);
+  div.appendChild(puntuacion);
+  div.appendChild(imagen);
 
-arrayHtmlElement.forEach(movieHtml => {divTarjetas.appendChild(movieHtml)
-    
+  return div;
+};
+
+$.get("https://students-api.up.railway.app/movies", (infoMovies) => {
+  const arrayHtmlElement = infoMovies.map(mapear);
+  arrayHtmlElement.forEach((movieHtml) => {
+    divTarjetas.appendChild(movieHtml);
+  });
 });
-
-
