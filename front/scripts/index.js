ce8546c2ecf67789c876d2066064ff9ab9fb47fd
pandreasantacruz <andreasantacruz123@gmail.com>
2024-12-18 20:48:48 -0600

    Tarea tres

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,21 +1,30 @@
-const divTarjetas = document.getElementById("misPeliculasFavoritas");
+const divTarjetas = document.getElementById("articleMisPeliculasFavoritas");
 
 const mapear = (movie) => {
   const div = document.createElement("div");
-  const titulo = document.createElement("strong");
-  const ano = document.createElement("span");
-  const direc = document.createElement("span");
-  const duracion = document.createElement("span");
-  const puntuacion = document.createElement("span");
+  const titulo = document.createElement("h5");
+  const ano = document.createElement("p");
+  const direc = document.createElement("p");
+  const duracion = document.createElement("p");
+  const puntuacion = document.createElement("p");
   const imagen = document.createElement("img");
 
+  div.classList.add("card-body");
+
   imagen.src = movie.poster;
+  imagen.classList.add("card-img-top");
   imagen.alt = movie.title;
+  imagen.classList.add("card-image-cap");
   puntuacion.innerText = movie.rate;
+  puntuacion.classList.add("card-text");
   duracion.innerText = movie.duration;
+  duracion.classList.add("card-text");
   direc.innerText = movie.director;
+  direc.classList.add("card-text");
   ano.innerText = movie.year;
+  ano.classList.add("card-text");
   titulo.innerText = movie.title;
+  titulo.classList.add("card-title");
 
   div.appendChild(titulo);
   div.appendChild(ano);
